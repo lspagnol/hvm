7499efbfbddf0e662768cfacdb0751a83e051eea
lspagnol <laurent.spagnol@univ-reims.fr>
2017-12-16 11:52:42 +0100

    indicateur hôte actif déplacé dans hv_pre-start

--- etc/hv_pre-start.sh
+++ etc/hv_pre-start.sh
@@ -2,3 +2,14 @@
 
 # Monter les dossiers utilisés par libvirt
 _zfs_mount
+
+# Indiquer le nom de l'hyperviseur actif dans le pool de stockage par défaut
+local _h
+chattr -i ${KVM_LIBVIRT_VARLIB_DIR}/images
+for _h in ${HVM_HOSTS} ; do
+	if [ -f ${_h} ] ; then
+		rm ${KVM_LIBVIRT_VARLIB_DIR}/${_h}
+	fi
+done
+touch "${KVM_LIBVIRT_VARLIB_DIR}/images/${HOSTNAME}"
+chattr +i ${KVM_LIBVIRT_VARLIB_DIR}/images
