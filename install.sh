4d7d8a367448f437e5835b0a02c76a601e8cee16
lspagnol <laurent.spagnol@univ-reims.fr>
2017-12-21 10:35:47 +0100

    ajout plugins munin

--- install.sh
+++ install.sh
@@ -11,8 +11,8 @@ chmod 750 /var/lib/hvm
 mkdir -p /usr/local/hvm/etc/
 cp -r etc/* /usr/local/hvm/etc/
 chown -R root:root /usr/local/hvm/etc/
-find /usr/local/hvm/etc/ -type d -exec chmod -R 750 {} \;
-find /usr/local/hvm/etc/ -type f -exec chmod -R 640 {} \;
+find /usr/local/hvm/etc/ -type d -exec chmod -R 755 {} \;
+find /usr/local/hvm/etc/ -type f -exec chmod -R 644 {} \;
 cp /usr/local/hvm/etc/common.conf.dist /usr/local/hvm/etc/common.conf
 [ -f /usr/local/hvm/etc/local.conf ] || cp /usr/local/hvm/etc/local.conf.dist /usr/local/hvm/etc/local.conf
 [ -f /usr/local/hvm/etc/hv_pre-start.sh ] || cp /usr/local/hvm/etc/hv_pre-start.sh.dist /usr/local/hvm/etc/hv_pre-start.sh
@@ -21,21 +21,37 @@ cp /usr/local/hvm/etc/common.conf.dist /usr/local/hvm/etc/common.conf
 mkdir -p /usr/local/hvm/sbin/
 cp sbin/* /usr/local/hvm/sbin/
 chown -R root:root /usr/local/hvm/sbin/
-find /usr/local/hvm/sbin/ -type d -exec chmod -R 750 {} \;
-find /usr/local/hvm/sbin/ -type f -exec chmod -R 640 {} \;
-chmod 750 /usr/local/hvm/sbin/hvm
-chmod 750 /usr/local/hvm/sbin/hvm-cron
-chmod 750 /usr/local/hvm/sbin/hvmd
-chmod 750 /usr/local/hvm/sbin/hvm-interface-config
-chmod 755 /usr/local/hvm/sbin/zpool_iostat_
+find /usr/local/hvm/sbin/ -type d -exec chmod -R 755 {} \;
+find /usr/local/hvm/sbin/ -type f -exec chmod -R 644 {} \;
+
+chmod 755 /usr/local/hvm/sbin/hvm
+chmod 755 /usr/local/hvm/sbin/hvm-cron
+chmod 755 /usr/local/hvm/sbin/hvmd
+chmod 755 /usr/local/hvm/sbin/hvm-interface-config
 ln -fs /usr/local/hvm/sbin/hvm /usr/local/sbin
 ln -fs /usr/local/hvm/sbin/hvm-interface-config /usr/local/sbin/
+
+# Script "maison"
+chmod 755 /usr/local/hvm/sbin/zpool_iostat_
+# https://github.com/munin-monitoring/contrib/blob/master/plugins/zfs/zfs_arcstats
+chmod 755 /usr/local/hvm/sbin/zfs_arcstats
+# https://github.com/munin-monitoring/contrib/blob/master/plugins/zfs/zpool_capacity
+chmod 755 /usr/local/hvm/sbin/zpool_capacity
 ln -fs /usr/local/hvm/sbin/zpool_iostat_ /etc/munin/plugins/zpool_iostat_bytes
 ln -fs /usr/local/hvm/sbin/zpool_iostat_ /etc/munin/plugins/zpool_iostat_iops
+ln -fs /usr/local/hvm/sbin/zfs_arcstats /etc/munin/plugins/zfs_arcstats
+ln -fs /usr/local/hvm/sbin/zpool_capacity /etc/munin/plugins/zpool_capacity
+cat<<EOF>/etc/munin/plugin-conf.d/zfs
+[zpool*]
+user root
+
+[zfs*]
+user root
+EOF
 service munin-node stop ; service munin-node start
 
 cp init/hvmd /etc/init.d
-chmod 750 /etc/init.d/hvmd
+chmod 755 /etc/init.d/hvmd
 update-rc.d -f hvmd defaults
 
 mkdir -p /usr/local/hvm/lib/
@@ -48,4 +64,3 @@ cp -r doc/* /usr/local/hvm/doc/
 find /usr/local/hvm/doc/ -type d -exec chmod -R 750 {} \;
 find /usr/local/hvm/doc/ -type f -exec chmod -R 640 {} \;
 test -h /usr/local/hvm/doc/hvm_help.txt || ln -s /usr/local/hvm/doc/hvm_help-fr.txt /usr/local/hvm/doc/hvm_help.txt
-
