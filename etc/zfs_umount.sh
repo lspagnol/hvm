35abbc6f17f7132d64800709b2f6b7a8d8b50aae
lspagnol <laurent.spagnol@univ-reims.fr>
2018-01-15 16:41:41 +0100

    suppression volume ZFS HVM

--- etc/zfs_umount.sh
+++ etc/zfs_umount.sh
@@ -2,8 +2,7 @@
 
 e=0
 
-#for v in /LIBVIRT/ISO /LIBVIRT/ROT /LIBVIRT/SSD /var/lib/hvm /var/lib/libvirt/qemu/save /var/lib/libvirt /etc/libvirt ; do
-for v in /LIBVIRT/ISO /LIBVIRT/ROT /LIBVIRT/SSD /var/lib/hvm /var/lib/libvirt /etc/libvirt ; do
+for v in /LIBVIRT/ISO /LIBVIRT/ROT /LIBVIRT/SSD /var/lib/libvirt /etc/libvirt ; do
 	cat /proc/mounts |grep -q " ${v} " 
 	if [ $? -eq 0 ] ; then
 		umount ${v} 2>/dev/null
