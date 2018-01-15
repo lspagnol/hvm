d631f788b4b550656e9e245f00db200fe4595e68
lspagnol <laurent.spagnol@univ-reims.fr>
2018-01-15 16:41:25 +0100

    suppression volume ZFS HVM

--- etc/zfs_mount.sh
+++ etc/zfs_mount.sh
@@ -1,8 +1,8 @@
 # Monter les dossiers utilisés par libvirt
 
+# Points de montage "legacy"
 mount -t zfs ROT/SHARED/libvirt/etc /etc/libvirt -o defaults,noatime,nodiratime
 mount -t zfs ROT/SHARED/libvirt/var_lib /var/lib/libvirt -o defaults,noatime,nodiratime
-#mount -t zfs SSD/SHARED/libvirt/qemu_save /var/lib/libvirt/qemu/save -o defaults,noatime,nodiratime
-mount -t zfs SSD/SHARED/hvm /var/lib/hvm -o defaults,noatime,nodiratime
 
+# Montage automatique
 zfs mount -a
